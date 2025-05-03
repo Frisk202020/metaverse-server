a6b3f436f9842c1875d7cea153301e87482aea8b
c.heme <>
2025-05-03 14:04:30 +0200

    final final

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let state = Arc::new(AppState {});
-    // let mut socket = "0.0.0.0:10000".parse::<SocketAddr>()?; 
-    let mut socket = "127.0.0.1:39751".parse::<SocketAddr>()?;
+    let mut socket = "0.0.0.0:10000".parse::<SocketAddr>()?; 
+    // let mut socket = "127.0.0.1:39751".parse::<SocketAddr>()?;
     let listener = tokio::net::TcpListener::bind(socket).await?;
     socket = listener.local_addr()?;
 
